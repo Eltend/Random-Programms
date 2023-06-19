ed5b36bc1a85bc3f2e7227d2562bedab38fe62ae
Eltend <67853698+Eltend@users.noreply.github.com>
2023-06-19 21:28:27 +0200

    first uneccescary programm
    it just adds 2 random numbers

---
+++ random_number.py
@@ -0,0 +1,12 @@
+import random
+
+#beginning test
+print("Hello World")
+
+#generate 2 random numbers and combine them
+randomNumber1 = random.randint(1,1000)
+randomNumber2 = random.randint(1,1000)
+total = randomNumber1 + randomNumber2
+
+#print the number
+print(total)
